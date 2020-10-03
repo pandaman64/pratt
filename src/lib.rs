d495d8cb75b32280b2d4510bd6d59d8320646dfc
pandaman64 <kointosudesuyo@infoseek.jp>
2020-10-03 22:41:57 +0900

    lossless syntax tree

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,46 @@
 use anyhow::*;
+use std::sync::Arc;
 
 use std::fmt;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum TokenKind {
+pub enum SyntaxKind {
     Whitespace,
     IntLit,
     Ident,
     Symbol,
+    Primitive,
+    PrefixOp(UnaryOp),
+    PostfixOp(UnaryOp),
+    BinOp(BinOp),
+    ParenOp(ParenOp),
+    QuantifierOp(QuantifierOp),
+    ExprRoot,
+}
+
+impl fmt::Display for SyntaxKind {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use SyntaxKind::*;
+
+        match self {
+            Whitespace => write!(f, "WS"),
+            IntLit => write!(f, "INTEGER"),
+            Ident => write!(f, "IDENT"),
+            Symbol => write!(f, "SYM"),
+            Primitive => write!(f, "PRIM"),
+            PrefixOp(_op) => write!(f, "PREFIX"),
+            PostfixOp(_op) => write!(f, "POSTFIX"),
+            BinOp(_op) => write!(f, "BINARY"),
+            ParenOp(_op) => write!(f, "PAREN"),
+            QuantifierOp(_op) => write!(f, "QUANTIFIER"),
+            ExprRoot => write!(f, "EXPR"),
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Token<'a> {
-    kind: TokenKind,
-    position: usize,
+    kind: SyntaxKind,
     value: &'a str,
 }
 
@@ -62,58 +89,81 @@ pub struct QuantifierOp {
     pub bp: u16,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum TreeKind<'a> {
-    IntLit(Token<'a>),
-    Ident(Token<'a>),
-    Symbol(Token<'a>),
-    PrefixOp(UnaryOp),
-    PostfixOp(UnaryOp),
-    BinOp(BinOp),
-    ParenOp(ParenOp),
-    QuantifierOp(QuantifierOp),
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum NodeOrToken<'a> {
+    Token(Arc<Token<'a>>),
+    Node(Arc<GreenNode<'a>>),
+}
+
+impl NodeOrToken<'_> {
+    fn text_width(&self) -> usize {
+        match self {
+            NodeOrToken::Token(token) => token.value.len(),
+            NodeOrToken::Node(node) => node.text_width,
+        }
+    }
+
+    fn kind(&self) -> SyntaxKind {
+        match self {
+            NodeOrToken::Token(token) => token.kind,
+            NodeOrToken::Node(node) => node.kind,
+        }
+    }
+}
+
+impl fmt::Display for NodeOrToken<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            NodeOrToken::Token(token) => write!(f, "{}", token.value),
+            NodeOrToken::Node(node) => write!(f, "{}", node),
+        }
+    }
+}
+
+impl<'a> From<Token<'a>> for NodeOrToken<'a> {
+    fn from(token: Token<'a>) -> Self {
+        NodeOrToken::Token(Arc::new(token))
+    }
+}
+
+impl<'a> From<GreenNode<'a>> for NodeOrToken<'a> {
+    fn from(node: GreenNode<'a>) -> Self {
+        NodeOrToken::Node(Arc::new(node))
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct CST<'a> {
-    kind: TreeKind<'a>,
-    // position: usize,
-    // len: usize,
-    children: Vec<CST<'a>>,
+pub struct GreenNode<'a> {
+    kind: SyntaxKind,
+    text_width: usize,
+    children: Vec<NodeOrToken<'a>>,
 }
 
-impl fmt::Display for CST<'_> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self.kind {
-            TreeKind::IntLit(token) | TreeKind::Ident(token) | TreeKind::Symbol(token) => {
-                assert!(self.children.is_empty());
-                write!(f, "{}", token.value)
-            }
-            TreeKind::PrefixOp(op) | TreeKind::PostfixOp(op) => {
-                assert_eq!(self.children.len(), 1);
-                write!(f, "({} {})", op.symbols, self.children[0])
-            }
-            TreeKind::BinOp(op) => {
-                assert_eq!(self.children.len(), 2);
-                write!(
-                    f,
-                    "({} {} {})",
-                    op.symbols, self.children[0], self.children[1]
-                )
-            }
-            TreeKind::ParenOp(op) => {
-                assert_eq!(self.children.len(), 1);
-                write!(f, "({} {})", op.op, self.children[0])
-            }
-            TreeKind::QuantifierOp(op) => {
-                assert_eq!(self.children.len(), 2);
-                write!(
-                    f,
-                    "({} {} {})",
-                    op.quantifier, self.children[0], self.children[1]
-                )
-            }
+impl<'a> GreenNode<'a> {
+    pub fn new(kind: SyntaxKind, children: Vec<NodeOrToken<'a>>) -> Self {
+        let text_width = children.iter().map(NodeOrToken::text_width).sum();
+        Self {
+            kind,
+            text_width,
+            children,
         }
+    }
+
+    pub fn non_trivial(&self) -> impl Iterator<Item = &NodeOrToken<'a>> + '_ {
+        self.children
+            .iter()
+            .filter(|child| child.kind() != SyntaxKind::Whitespace)
+    }
+}
+
+// TODO: fix
+impl fmt::Display for GreenNode<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}", self.kind)?;
+        for child in self.non_trivial() {
+            write!(f, " {}", child)?;
+        }
+        write!(f, ")")
     }
 }
 
@@ -163,18 +213,16 @@ impl<'a> Parser<'a> {
                 let value = input.split(|c| c < '0' || c > '9').next().unwrap();
 
                 Token {
-                    kind: TokenKind::IntLit,
+                    kind: SyntaxKind::IntLit,
                     value,
-                    position: self.position,
                 }
             }
             c if c.is_whitespace() => {
                 let value = input.split(|c: char| !c.is_whitespace()).next().unwrap();
 
                 Token {
-                    kind: TokenKind::Whitespace,
+                    kind: SyntaxKind::Whitespace,
                     value,
-                    position: self.position,
                 }
             }
             c if c.is_alphabetic() => {
@@ -184,15 +232,13 @@ impl<'a> Parser<'a> {
                     .unwrap();
 
                 Token {
-                    kind: TokenKind::Ident,
+                    kind: SyntaxKind::Ident,
                     value,
-                    position: self.position,
                 }
             }
             c => Token {
-                kind: TokenKind::Symbol,
+                kind: SyntaxKind::Symbol,
                 value: &input[0..c.len_utf8()],
-                position: self.position,
             },
         };
 
@@ -203,123 +249,123 @@ impl<'a> Parser<'a> {
         self.peek_token_at(0)
     }
 
-    fn expect_token(&mut self, token: Token<'a>) -> Result<()> {
+    fn expect_token(
+        &mut self,
+        token: Token<'a>,
+        children: &mut Vec<NodeOrToken<'a>>,
+    ) -> Result<()> {
         ensure!(
             matches!(self.peek_token(), Ok(t) if token == t),
             "expected {:?}, got {}",
             token,
             self.remaining()
         );
-        self.next_token().unwrap();
+        self.next_token(children).unwrap();
         Ok(())
     }
 
-    fn next_token(&mut self) -> Result<Token<'a>> {
+    fn next_token(&mut self, children: &mut Vec<NodeOrToken<'a>>) -> Result<()> {
         let token = self.peek_token()?;
         self.advance(token.value.len());
-        Ok(token)
+        children.push(token.into());
+        Ok(())
     }
 
-    fn skip_ws(&mut self) {
+    fn next_ident(&mut self, children: &mut Vec<NodeOrToken<'a>>) -> Result<()> {
+        let token = self.peek_token()?;
+        ensure!(
+            token.kind == SyntaxKind::Ident,
+            "expected identifier, got {:?}",
+            token
+        );
+        self.advance(token.value.len());
+        children.push(token.into());
+        Ok(())
+    }
+
+    fn skip_ws(&mut self, children: &mut Vec<NodeOrToken<'a>>) {
         if let Ok(token) = self.peek_token() {
-            if token.kind == TokenKind::Whitespace {
-                self.expect_token(token).unwrap();
+            if token.kind == SyntaxKind::Whitespace {
+                self.expect_token(token, children).unwrap();
             }
         }
     }
 
-    fn expect(&mut self, target: &str) -> Result<Token<'a>> {
+    fn expect(&mut self, target: &str, children: &mut Vec<NodeOrToken<'a>>) -> Result<()> {
         ensure!(
             self.remaining().starts_with(target),
             "expected {}, got {}",
             target,
             self.remaining(),
         );
+
         let token = Token {
-            kind: TokenKind::Symbol,
-            position: self.position,
+            kind: SyntaxKind::Symbol,
             value: &self.remaining()[0..target.len()],
         };
+        children.push(token.into());
+
         self.advance(target.len());
-        Ok(token)
+        Ok(())
     }
 
-    fn primary_expr(&mut self) -> Result<CST<'a>> {
+    fn primary_expr(&mut self) -> Result<GreenNode<'a>> {
         match self.peek_token()? {
-            token if token.kind == TokenKind::IntLit => {
-                self.expect_token(token).unwrap();
-                Ok(CST {
-                    kind: TreeKind::IntLit(token),
-                    // position: token.position,
-                    // len: token.value.len(),
-                    children: vec![],
-                })
-            }
-            token if token.kind == TokenKind::Ident => {
-                self.expect_token(token).unwrap();
-                Ok(CST {
-                    kind: TreeKind::Ident(token),
-                    // position: token.position,
-                    // len: token.value.len(),
-                    children: vec![],
-                })
+            token if token.kind == SyntaxKind::IntLit || token.kind == SyntaxKind::Ident => {
+                let mut children = vec![];
+                self.expect_token(token, &mut children).unwrap();
+                Ok(GreenNode::new(SyntaxKind::Primitive, children))
             }
             token => bail!("expected primary expr, got {:?}", token),
         }
     }
 
-    pub fn expr(&mut self, min_bp: u16) -> Result<CST<'a>> {
+    pub fn expr(&mut self, min_bp: u16) -> Result<GreenNode<'a>> {
         // prefix
         let mut lhs = if let Some(op) = self.peek_quantifierfix() {
-            self.expect(op.quantifier).unwrap();
-            self.skip_ws();
+            let mut children = vec![];
+            self.expect(op.quantifier, &mut children).unwrap();
+            self.skip_ws(&mut children);
 
-            let token = self.next_token()?;
-            ensure!(
-                token.kind == TokenKind::Ident,
-                "expected ident, got {:?}",
-                token
-            );
-            let ident = CST {
-                kind: TreeKind::Ident(token),
-                children: vec![],
-            };
-            self.skip_ws();
+            self.next_ident(&mut children)?;
+            self.skip_ws(&mut children);
 
-            self.expect(op.separator)?;
-            self.skip_ws();
+            self.expect(op.separator, &mut children)?;
+            self.skip_ws(&mut children);
 
-            let inner = self.expr(op.bp)?;
+            let node = self.expr(op.bp)?;
+            children.push(node.into());
 
-            CST {
-                kind: TreeKind::QuantifierOp(op),
-                children: vec![ident, inner],
-            }
+            GreenNode::new(SyntaxKind::QuantifierOp(op), children)
         } else if let Some(op) = self.peek_prefix() {
-            self.expect(op.symbols).unwrap();
-            self.skip_ws();
-            let inner = self.expr(op.bp)?;
+            let mut children = vec![];
 
-            CST {
-                kind: TreeKind::PrefixOp(op),
-                children: vec![inner],
-            }
+            self.expect(op.symbols, &mut children).unwrap();
+            self.skip_ws(&mut children);
+
+            let node = self.expr(op.bp)?;
+            children.push(node.into());
+
+            GreenNode::new(SyntaxKind::PrefixOp(op), children)
         } else if let Some(op) = self.peek_parenfix() {
-            self.expect(op.open_symbols).unwrap();
-            self.skip_ws();
-            let inner = self.expr(0)?;
-            self.skip_ws();
-            self.expect(op.close_symbols)?;
-            CST {
-                kind: TreeKind::ParenOp(op),
-                children: vec![inner],
-            }
+            let mut children = vec![];
+
+            self.expect(op.open_symbols, &mut children).unwrap();
+            self.skip_ws(&mut children);
+
+            let node = self.expr(0)?;
+            children.push(node.into());
+            self.skip_ws(&mut children);
+
+            self.expect(op.close_symbols, &mut children)?;
+            GreenNode::new(SyntaxKind::ParenOp(op), children)
         } else {
             self.primary_expr()?
         };
 
         loop {
-            self.skip_ws();
+            let mut spaces = vec![];
+            self.skip_ws(&mut spaces);
 
             if self.remaining().is_empty() {
                 break;
@@ -330,12 +376,12 @@ impl<'a> Parser<'a> {
                     break;
                 }
 
-                self.expect(op.symbols).unwrap();
-                self.skip_ws();
-                lhs = CST {
-                    kind: TreeKind::PostfixOp(op),
-                    children: vec![lhs],
-                };
+                let mut children = vec![];
+                children.push(lhs.into());
+                children.extend(spaces);
+                self.expect(op.symbols, &mut children).unwrap();
+                self.skip_ws(&mut children);
+                lhs = GreenNode::new(SyntaxKind::PostfixOp(op), children);
                 continue;
             }
 
@@ -344,14 +390,14 @@ impl<'a> Parser<'a> {
                     break;
                 }
 
-                // consume only if parsing the right hand side
-                self.expect(&op.symbols).unwrap();
-                self.skip_ws();
+                let mut children = vec![];
+                children.push(lhs.into());
+                children.extend(spaces);
+                self.expect(&op.symbols, &mut children).unwrap();
+                self.skip_ws(&mut children);
                 let rhs = self.expr(op.rbp())?;
-                lhs = CST {
-                    kind: TreeKind::BinOp(op),
-                    children: vec![lhs, rhs],
-                };
+                children.push(rhs.into());
+                lhs = GreenNode::new(SyntaxKind::BinOp(op), children);
                 continue;
             }
 
@@ -431,8 +477,8 @@ impl<'a> Parser<'a> {
 mod test {
     use super::*;
 
-    fn success_complete(input: &str, expected: &str) {
-        let language = Language {
+    fn common_language() -> Language {
+        Language {
             infix_table: vec![
                 BinOp {
                     symbols: "=",
@@ -480,66 +526,113 @@ mod test {
                     bp: 10,
                 },
             ],
-        };
+        }
+    }
+
+    fn success_complete(language: Language, input: &str, expected: &str) {
+        let _ = tracing_subscriber::fmt::try_init();
+
         let mut parser = Parser::new(input, language);
         let e = parser.expr(0).unwrap();
-        eprintln!("{}", e.to_string());
-        assert_eq!(e.to_string(), expected);
+        tracing::debug!(%e);
+        assert_eq!(
+            e.to_string(),
+            expected,
+            "parse result doesn't match: {:?}",
+            e
+        );
         assert!(
             parser.remaining().is_empty(),
             "input must be consumed, remaining: {}",
             parser.remaining()
         );
+        // assert_eq!(e.text_width, input.len(),);
     }
 
     #[test]
     fn test_intlit() {
-        success_complete("12345", "12345");
+        success_complete(common_language(), "12345", "(PRIM 12345)");
     }
 
     #[test]
     fn test_binop() {
-        success_complete("123 + 45", "(+ 123 45)");
+        success_complete(
+            common_language(),
+            "123 + 45",
+            "(BINARY (PRIM 123) + (PRIM 45))",
+        );
     }
 
     #[test]
     fn test_left_assoc() {
-        success_complete("123 + 45 + 67", "(+ (+ 123 45) 67)");
+        success_complete(
+            common_language(),
+            "123 + 45 + 67",
+            "(BINARY (BINARY (PRIM 123) + (PRIM 45)) + (PRIM 67))",
+        );
     }
 
     #[test]
     fn test_right_assoc() {
-        success_complete("2^3^4", "(^ 2 (^ 3 4))");
+        success_complete(
+            common_language(),
+            "2^3^4",
+            "(BINARY (PRIM 2) ^ (BINARY (PRIM 3) ^ (PRIM 4)))",
+        );
     }
 
     #[test]
     fn test_prefix() {
-        success_complete("-2 + 5 + -4 ^ 2", "(+ (+ (- 2) 5) (- (^ 4 2)))");
+        success_complete(
+            common_language(),
+            "-2 + 5 + -4 ^ 2",
+            "(BINARY (BINARY (PREFIX - (PRIM 2)) + (PRIM 5)) + (PREFIX - (BINARY (PRIM 4) ^ (PRIM 2))))"
+        );
     }
 
     #[test]
     fn test_postfix() {
-        success_complete("1 - -2! + 3", "(+ (- 1 (- (! 2))) 3)");
+        success_complete(
+            common_language(),
+            "1 - -2! + 3",
+            "(BINARY (BINARY (PRIM 1) - (PREFIX - (POSTFIX (PRIM 2) !))) + (PRIM 3))",
+        );
     }
 
     #[test]
     fn test_paren() {
-        success_complete("-(2 + 5 + -4)^2", "(- (^ (paren (+ (+ 2 5) (- 4))) 2))");
+        success_complete(
+            common_language(),
+            "-(2 + 5 + -4)^2",
+            "(PREFIX - (BINARY (PAREN ( (BINARY (BINARY (PRIM 2) + (PRIM 5)) + (PREFIX - (PRIM 4))) )) ^ (PRIM 2)))"
+        );
     }
 
     #[test]
     fn test_ident() {
-        success_complete("a^3 + b^3 - c^3", "(- (+ (^ a 3) (^ b 3)) (^ c 3))")
+        success_complete(
+            common_language(),
+            "a^3 + b^3 - c^3",
+            "(BINARY (BINARY (BINARY (PRIM a) ^ (PRIM 3)) + (BINARY (PRIM b) ^ (PRIM 3))) - (BINARY (PRIM c) ^ (PRIM 3)))",
+        )
     }
 
     #[test]
     fn test_ident_underscore() {
-        success_complete("a_1 + b__", "(+ a_1 b__)")
+        success_complete(
+            common_language(),
+            "a_1 + b__",
+            "(BINARY (PRIM a_1) + (PRIM b__))",
+        )
     }
 
     #[test]
-    fn test_multichar_op() {
-        success_complete("a^3 + b^3 = c^3", "(= (+ (^ a 3) (^ b 3)) (^ c 3))")
+    fn test_equal_op() {
+        success_complete(
+            common_language(),
+            "a^3 + b^3 = c^3",
+            "(BINARY (BINARY (BINARY (PRIM a) ^ (PRIM 3)) + (BINARY (PRIM b) ^ (PRIM 3))) = (BINARY (PRIM c) ^ (PRIM 3)))",
+        )
     }
 
     #[test]
@@ -559,10 +652,11 @@ mod test {
             ],
             ..Language::default()
         };
-        let mut parser = Parser::new("Γ ⊢t pre -> post", language);
-        let e = parser.expr(0).unwrap();
-        assert_eq!(e.to_string(), "(⊢t Γ (-> pre post))");
-        assert!(parser.remaining().is_empty());
+        success_complete(
+            language,
+            "Γ ⊢t pre -> post",
+            "(BINARY (PRIM Γ) ⊢t (BINARY (PRIM pre) -> (PRIM post)))",
+        )
     }
 
     #[test]
@@ -580,14 +674,19 @@ mod test {
             }],
             ..Language::default()
         };
-        let mut parser = Parser::new("[| t |] = 100", language);
-        let e = parser.expr(0).unwrap();
-        assert_eq!(e.to_string(), "(= (denotation t) 100)");
-        assert!(parser.remaining().is_empty());
+        success_complete(
+            language,
+            "[| t |] = 100",
+            "(BINARY (PAREN [| (PRIM t) |]) = (PRIM 100))",
+        )
     }
 
     #[test]
     fn test_quantifier() {
-        success_complete("∀y. (fun x -> y) = fun z -> y", "(∀ y (= (paren (fun x y)) (fun z y)))")
+        success_complete(
+            common_language(),
+            "∀y. (fun x -> y) = fun z -> y",
+            "(QUANTIFIER ∀ y . (BINARY (PAREN ( (QUANTIFIER fun x -> (PRIM y)) )) = (QUANTIFIER fun z -> (PRIM y))))",
+        )
     }
 }
